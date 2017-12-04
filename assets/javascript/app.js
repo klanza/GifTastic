ed5b18b8def42083b87de3dde559c3f258add11a
klanza <klanza91@gmail.com>
2017-12-04 01:12:39 -0700

    Created array of topics, created functions to generate buttons from array

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+let topics = ["Dota 2", "Pokemon", "Mario", "Gears of War", "The Last of Us", "Sonic", "Metroid", "Counter-Strike", "Halo",
+              "Dark Souls", "Bloodborne", "The Legend of Zelda", "Doom", "Portal", "Half-Life", "Tetris", "Final Fantasy", 
+              "Grand Theft Auto", "Street Fighter", "Megaman", "Metal Gear Solid", "Castlevania", "Warcraft", "Bioshock"]
+
+let createTopicButton = function(game){
+    // Create button
+    let btn = $("<button>")
+        // Add class to button
+        .addClass("game-topic m-1")
+        // Add data-title attribute
+        .attr("data-title", game)
+        // Create text inside the HTML element
+        .text(game)
+    // Return button to insert into DOM
+    return btn
+
+}
+
+let renderTopicButton = function() {
+    topics.forEach((value) => {
+        $("#topic-buttons").append(createTopicButton(value))
+    });
+}
+
+renderTopicButton();
